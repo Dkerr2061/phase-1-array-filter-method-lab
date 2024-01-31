d446c2857bc24060b5dbead8bc8d5422048eead2
David Kerr <kerrky@gmail.com>
2024-01-31 14:37:22 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+
+
+function findMatching(drivers, string) {
+  let lowerCased = string.toLowerCase();
+  return drivers.filter( element => {
+    return element.toLowerCase().indexOf(lowerCased) !== -1;
+  });
+}
+
+function fuzzyMatch(drivers, string) {
+  let lowerCased = string.toLowerCase();
+
+  return drivers.filter( element => {
+    return element.toLowerCase().slice(0, 2) === lowerCased;
+  })
+}
+
+ function matchName(drivers, string) {
+  const filtered = drivers.filter( driver => {
+    return (driver.name === string) 
+  });
+  return filtered;
+ }
+
+
+
